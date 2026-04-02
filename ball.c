e28da1fff7ddabde8712f3f7f999efddcae700e2
Mitchell Bravo <mitchellbravo007@gmail.com>
2026-04-02 00:37:54 -0400

    Added velocity and acceleration

--- ball.c
+++ ball.c
@@ -11,15 +11,31 @@ void ballMovement(SDL_Rect *ball, Uint32 clearcolor, Uint32 color, SDL_Surface *
     ball->y += vel_y;
 
     // top and bottom wall bounce
-    if (ball->y < 0) vel_y = -vel_y;
-    if (ball->y + ball->h > 480) vel_y = -vel_y;
+    if (ball->y < 0) // top 
+    {
+        vel_y = -vel_y;
+    }
+    if (ball->y + ball->h > 480) // bottom
+    {
+        vel_y = -vel_y;
+    }
 
     // left and right wall bounce (reset later)
     if (ball->x < 0 || ball->x + ball->w > 640) vel_x = -vel_x;
 
     // paddle collision
-    if (SDL_HasIntersection(ball, p1)) vel_x = -vel_x;
-    if (SDL_HasIntersection(ball, p2)) vel_x = -vel_x;
+    if (SDL_HasIntersection(ball, p1))
+    {
+        vel_x = -vel_x;
+        vel_x++;
+        printf("%d\n", vel_x);
+    }
+    if (SDL_HasIntersection(ball, p2))
+    {
+        vel_x = -vel_x;
+        vel_x--;
+        printf("%d\n", vel_x);
+    }
 
     SDL_FillRect(surface, ball, color);
 }
